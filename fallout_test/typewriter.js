68a5c3a71aac9393c4140f8c7bc96089a8dd20b5
Diego Kaiser <diegok99@hotmail.ch>
2023-05-14 19:50:34 +0200

    Added typewriter output

--- fallout_test/typewriter.js
+++ fallout_test/typewriter.js
@@ -1,49 +1,59 @@
 import * as qtexts from "./texts.js";
-//const quest_text_power = qtexts.quest_text("power")
 
-// set up text to print, each item in array is new line
-    let iSpeed = 100; // time delay of print out
-    let iIndex = 0; // start printing array at this position
-    let iScrollAt = 20; // start scrolling up at this many lines
-     
-    let iTextPos = 0; // initialise text position
-    let sContents = ''; // initialise contents variable
-    let iRow; // initialise current row
+    const destination = document.getElementById("typedtext");
+
+    const iSpeed = 100; // time delay of print out
+
+    let uglyArray = new Array();
      
     function typewriter()
     {
-        // Why? iRow = Math.max(0, iIndex-iScrollAt);
         let aText = getDataFromArray();
-
-        aText.forEach(ex => {
-            ex += "<br/>";
-            sContents += ex;
-        });
-        //console.log(sContents)
-
-        let destination = document.getElementById("typedtext");
-        destination.innerHTML = sContents;
-
-        // while ( iRow < iIndex ) {
-        //     sContents += aText[iRow++] + '<br />';
-        // }
-    
-        //  destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
-        let iArrLength = aText.length; // the length of the text array
-        if ( iTextPos++ == iArrLength ) {
-            iTextPos = 0;
-            iIndex++;
-            if ( iIndex != aText.length ) {
-                iArrLength = aText[iIndex].length;
-                setTimeout("typewriter()", 500);
-            }
-        } else {
-            setTimeout("typewriter()", iSpeed);
-        }
+        writeAllLines(aText, 0);
     }
 
     function getDataFromArray() {
         return qtexts.quest_text("power");
+    }
+
+    function showText(message, index, interval) {
+        if (index <= message.length) {
+            if(uglyArray.includes(index)) {
+                destination.appendChild(document.createElement("br"));
+            }
+            if(message[index] === undefined) {
+                return;
+            }
+
+            let tester = message[index];
+            destination.append(message[index++]);
+            setTimeout(function () { showText(message, index, interval); }, interval);
+        }
+    }
+
+    function loopThroughArray (arr, index) {
+        let startIndex = 0;
+
+        let uglyString = "";
+
+        
+        for(let element of arr) {
+            let lastElementInUglyArray = uglyArray[uglyArray.length-1];
+            if(lastElementInUglyArray === undefined) {
+                uglyArray.push(element.length);
+            }
+            else {
+                uglyArray.push(lastElementInUglyArray + element.length);
+            }
+            uglyString += element;
+        };
+        return uglyString;
+    }
+
+    function writeAllLines(arrayOfStrings, index) {
+        let startIndex = 0;
+        let allLines = loopThroughArray(arrayOfStrings, index);
+        showText (allLines, 0, iSpeed);
     }
     
     typewriter();
