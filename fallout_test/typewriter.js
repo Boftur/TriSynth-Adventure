d7800a11dcdf0b25d27d6e7567eb38a69f52fc2c
Diego Kaiser <diegok99@hotmail.ch>
2023-05-14 14:40:58 +0200

    Fixed imports

--- fallout_test/typewriter.js
+++ fallout_test/typewriter.js
@@ -1,38 +1,49 @@
-import * as qtexts from "./texts";
-const quest_text_power = texts.quest_text("power")
+import * as qtexts from "./texts.js";
+//const quest_text_power = qtexts.quest_text("power")
 
 // set up text to print, each item in array is new line
-    var aText = quest_text_power;
-    var iSpeed = 100; // time delay of print out
-    var iIndex = 0; // start printing array at this posision
-    var iArrLength = aText[0].length; // the length of the text array
-    var iScrollAt = 20; // start scrolling up at this many lines
+    let iSpeed = 100; // time delay of print out
+    let iIndex = 0; // start printing array at this position
+    let iScrollAt = 20; // start scrolling up at this many lines
      
-    var iTextPos = 0; // initialise text position
-    var sContents = ''; // initialise contents variable
-    var iRow; // initialise current row
+    let iTextPos = 0; // initialise text position
+    let sContents = ''; // initialise contents variable
+    let iRow; // initialise current row
      
     function typewriter()
     {
-     sContents =  ' ';
-     iRow = Math.max(0, iIndex-iScrollAt);
-     var destination = document.getElementById("typedtext");
-     
-     while ( iRow < iIndex ) {
-      sContents += aText[iRow++] + '<br />';
-     }
-     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
-     if ( iTextPos++ == iArrLength ) {
-      iTextPos = 0;
-      iIndex++;
-      if ( iIndex != aText.length ) {
-       iArrLength = aText[iIndex].length;
-       setTimeout("typewriter()", 500);
-      }
-     } else {
-      setTimeout("typewriter()", iSpeed);
-     }
-    }
+        // Why? iRow = Math.max(0, iIndex-iScrollAt);
+        let aText = getDataFromArray();
+
+        aText.forEach(ex => {
+            ex += "<br/>";
+            sContents += ex;
+        });
+        //console.log(sContents)
+
+        let destination = document.getElementById("typedtext");
+        destination.innerHTML = sContents;
+
+        // while ( iRow < iIndex ) {
+        //     sContents += aText[iRow++] + '<br />';
+        // }
     
+        //  destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+        let iArrLength = aText.length; // the length of the text array
+        if ( iTextPos++ == iArrLength ) {
+            iTextPos = 0;
+            iIndex++;
+            if ( iIndex != aText.length ) {
+                iArrLength = aText[iIndex].length;
+                setTimeout("typewriter()", 500);
+            }
+        } else {
+            setTimeout("typewriter()", iSpeed);
+        }
+    }
+
+    function getDataFromArray() {
+        return qtexts.quest_text("power");
+    }
     
     typewriter();
