13ac2850cb10b8271b5e6866a108d3cae5d10e87
Boftur <alfrededvin@gmail.com>
2023-05-17 19:02:44 +0200

    Update texts.js

--- plugins/texts.js
+++ plugins/texts.js
@@ -1,24 +1,18 @@
-
-
-export function quest_text(quest_place) {
-    if (quest_place == "Gogh")
+export function quest_text(user_input) {
+    if ("user_input" == "power")
 
         // Leave this console output, or everything breaks
         console.log(" ");
 
-        return Array(
-            "Welcome to the real world, Neo.",
-            "Wanna take a red 'un or a blu 'un",
-            "Yo how's it goin",
-        );
+        // This returns an array with 25 numbered entries for easy controlling
+        let array = new Array();
 
-
-
-
-    if ("destination" == "Painting")
-
-        // Leave this console output, or everything breaks
-        console.log(" ");
+        for(let i = 1; i <= 25; i++) {
+            let numberedLine = i.toString().repeat(20);
+            array.push(numberedLine);
+        }
+        
+        return array;
 
         return Array(
             "Welcome to the real world, Neo.",
